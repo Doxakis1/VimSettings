5b31122a6544dc699d9e080dacb96e51bc0c66e2
Michail K. <112430294+Doxakis1@users.noreply.github.com>
2024-10-05 15:40:55 +0300

    Update Install.sh

--- NvimConfig/Install.sh
+++ NvimConfig/Install.sh
@@ -5,7 +5,7 @@ sudo apt install -y bear tmux gdb build-essential git gcc vim clang lldb clangd 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
-sudo echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' > ~/.bashrc
+sudo echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
 sudo rm -rf ~/.config/nvim
 sudo cp -R nvim ~/.config
-sudo echo 'alias vim="nvim"' > ~/.bashrc
+sudo echo 'alias vim="nvim"' >> ~/.bashrc

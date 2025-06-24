99ed8a01e8720abd1972a250def73dd5115d1358
Michail Karatzidis <mike.karat@yahoo.com>
2025-06-24 11:08:04 +0300

    Update Nvim Install script

--- NvimConfig/Install.sh
+++ NvimConfig/Install.sh
@@ -2,9 +2,9 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y bear tmux gdb build-essential git gcc vim clang lldb clangd gcc-multilib ddd sasm nasm
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/download/v0.11.2/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 sudo echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
 sudo rm -rf ~/.config/nvim
 sudo cp -R nvim ~/.config

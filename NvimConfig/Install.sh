a9646b476c7a57f010b7762d6d0d6beff2493f9b
Michail Karatzidis <mike.karat@yahoo.com>
2025-06-24 11:15:58 +0300

    Update PATH variable

--- NvimConfig/Install.sh
+++ NvimConfig/Install.sh
@@ -5,7 +5,7 @@ sudo apt install -y bear tmux gdb build-essential git gcc vim clang lldb clangd 
 curl -LO https://github.com/neovim/neovim/releases/download/v0.11.2/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-sudo echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
+sudo echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.bashrc
 sudo rm -rf ~/.config/nvim
 sudo cp -R nvim ~/.config
 sudo echo 'alias vim="nvim"' >> ~/.bashrc

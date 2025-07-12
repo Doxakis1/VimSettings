3cb0b4a8d412f4054259d83afadd1b7359b3a267
Michail Karatzidis <mike.karat@yahoo.com>
2025-07-13 01:48:02 +0300

    Update record_video script to add keyboard capture

--- record_video.sh
+++ record_video.sh
@@ -5,10 +5,12 @@ RECORDING_PID=$(cat $RECORDING_FILE 2>/dev/null)
 if [[ -z "$RECORDING_PID" || ! "$RECORDING_PID" =~ ^[0-9]+$ ]]; then
     ffmpeg -f x11grab -video_size 1920x1080 -i :0.0 -f alsa -i default -af:a "afftdn=nf=-75" -c:v libx264 -preset ultrafast -crf 23 -c:a aac -b:a 128k /home/$USER/$(date +"%d_%m_%H_%M_%S").mp4 &>/dev/null &
     pid=$!
+    screenkey -s medium -p bottom --opacity 0.5 &>/dev/null &
     echo "$pid" > "$RECORDING_FILE"
     echo "Started recording process with PID $pid"
 else
     echo "Stopping recording process $RECORDING_PID"
+    pkill screenkey
     kill -15 "$RECORDING_PID"
     rm "$RECORDING_FILE"
 fi

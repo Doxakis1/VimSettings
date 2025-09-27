a8df53c8bac05b6c46232b67a345cdfa56125004
Michail Karatzidis <mike.karat@yahoo.com>
2025-09-27 19:16:46 +0300

    Added golang lsp to nvim settings
    Added golang lsp to nvim settings

--- NvimConfig/nvim/lua/plugins/lspconfig.lua
+++ NvimConfig/nvim/lua/plugins/lspconfig.lua
@@ -1,13 +1,30 @@
 return {
-    "neovim/nvim-lspconfig",
-    name = "lspconfig",
-    config = function()
-        local configs = require("lspconfig")
-        configs.clangd.setup{
-            on_attach = function()
-                vim.keymap.set('n', 'gh', ':ClangdSwitchSourceHeader<cr>', { silent = true })
-            end,
-        }
-		configs.pyright.setup{}
-    end
+  "neovim/nvim-lspconfig",
+  name = "lspconfig",
+  config = function()
+    local configs = require("lspconfig")
+
+    -- C/C++
+    configs.clangd.setup({
+      on_attach = function()
+        vim.keymap.set("n", "gh", ":ClangdSwitchSourceHeader<cr>", { silent = true })
+      end,
+    })
+
+    -- Python
+    configs.pyright.setup({})
+
+    -- Golang
+    configs.gopls.setup({
+      settings = {
+        gopls = {
+          analyses = { unusedparams = true },
+          staticcheck = true,
+          gofumpt = true,
+          completeUnimported = true,
+          usePlaceholders = true,
+        },
+      },
+    })
+  end,
 }
